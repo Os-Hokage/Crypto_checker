ed0862213fb5bfc019c9e02dc95af63be49bca4d
Os-Hokage <controlaola000@gmail.com>
2022-09-08 14:46:09 +0100

    Add existing project files to Git

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          useMaterial3: true,
+          iconTheme: const IconThemeData(color: Colors.white),
+          fontFamily: 'poppins',
+          textTheme: const TextTheme(
+              titleLarge:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              bodyLarge: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15),
+              bodyMedium: TextStyle(color: Colors.white),
+              bodySmall: TextStyle(color: Colors.white60, fontSize: 14))),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
